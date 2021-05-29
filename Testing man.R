1eac8bb8b13845b23597da89ece8bf9f493752e2
BinhNg <binhnguyen.fsh@gmail.com>
2021-05-30 06:57:11 +0700

    Hey

---
+++ Testing man.R
@@ -0,0 +1,5 @@
+print("This file was created within RStudio")
+print("And now it lives on GitHub")
+
+git config --global user.email "binhnguyen.fsh@gmail.com"
+git config --global user.name "BinhNg"
